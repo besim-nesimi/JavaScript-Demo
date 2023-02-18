caa01dbb17c4ddcc96fe5ba1aa9232976b126714
bestronaut <nesimibesim@outlook.com>
2023-02-18 20:01:00 +0100

    Initial commit

---
+++ app.js
@@ -0,0 +1,19 @@
+console.log("Hej från Javascript");
+
+const person = { name: "Besim", age: 28 };
+
+person.favoriteFood = "Hamburger"
+
+console.log(person)
+
+console.log(person.name)
+console.log(person.age)
+console.log(person.favoriteFood)
+
+let variable
+
+console.log(variable)
+
+function printHello() {
+    console.log("Hello")
+    }
